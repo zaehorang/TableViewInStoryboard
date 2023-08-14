af21442455d88bf84e3bc67eafafefb5e2c9f24d
zaehorang <zaehorang2@naver.com>
2023-08-14 16:33:01 +0900

    Initial Commit

---
+++ TableViewInStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewInStoryboard
+//
+//  Created by zaehorang on 2023/08/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
