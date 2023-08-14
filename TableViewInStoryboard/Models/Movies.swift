2b6eaebcbb65979975aa0b6ee2f5b6b40393eaba
zaehorang <zaehorang2@naver.com>
2023-08-15 01:08:30 +0900

    화면 구성 완료

---
+++ TableViewInStoryboard/Models/Movies.swift
@@ -0,0 +1,14 @@
+//
+//  Movies.swift
+//  TableViewInStoryboard
+//
+//  Created by zaehorang on 2023/08/14.
+//
+
+import UIKit
+
+struct Movie {
+    var movieImage: UIImage?
+    let movieName: String
+    let movieDescription: String
+}
